81ce9e10dda614bbcac52b25d0352f11dee54ea7
Greatness Emmanuel-king <greatnessking2003@gmail.com>
2022-09-01 21:56:28 -0400

    8th commit

---
+++ src/components/CourseList.js
@@ -0,0 +1,42 @@
+import Course from "./Course.js";
+import React, { useState } from "react";
+import { terms } from "../utilities/times.js";
+import { getCourseTerm } from "./Course.js";
+
+const TermButton = ({term, setTerm, checked}) => (
+    <>
+      <input type="radio" id={term} className="btn-check" autoComplete="off" 
+       checked={checked} onChange={()=> setTerm(term)} />
+      <label className="btn btn-sucess m-1 p-2" htmlFor={term}>
+      {term}
+      </label>
+    </>
+  );
+  
+  const TermSelector = ({term, setTerm}) => (
+    <div className="btn-group">
+    {
+      Object.values(terms).map(value => (
+        <TermButton key={value} term={value} setTerm={setTerm} checked={value===term}/>
+      ))
+    }  
+    </div>
+  );
+  
+const CourseList = ({ courses }) => {
+const [term, setTerm] = useState('Fall');
+const [selected, setSelected] = useState([]);
+const termCourses = Object.values(courses).filter(course=>term===getCourseTerm(course));
+return (
+    <>
+    <TermSelector term={term} setTerm={setTerm} />
+    <div className="course-list">
+    { termCourses.map(course => 
+        <Course key={course.id} course={ course } selected={selected} setSelected={ setSelected }
+        />) }
+    </div>
+    </>
+);
+};
+
+export default CourseList;
